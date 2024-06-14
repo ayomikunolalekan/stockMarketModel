19111cdfbfc3d862b075b611277ef65d5a3962bb
Quyum Kehinde <quyumkehinde@gmail.com>
2024-06-14 01:07:26 +0100

    fix: file fetching bug

--- web-deployment/app.py
+++ web-deployment/app.py
@@ -4,7 +4,7 @@ import joblib
 app = Flask(__name__)
 
 # Load the saved ARIMA model
-model = joblib.load("arima_model.pkl")
+model = joblib.load("model.pkl")
 
 
 @app.route("/predict", methods=["POST", "GET"])
